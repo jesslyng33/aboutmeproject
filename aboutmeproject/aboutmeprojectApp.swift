f6fcdc05a663ee4ceeafeed90ffab45d27171d27
Jesslyn Gunadi <jesslyngunadi33@gmail.com>
2023-08-10 13:13:06 -0400

    Initial Commit

---
+++ aboutmeproject/aboutmeprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  aboutmeprojectApp.swift
+//  aboutmeproject
+//
+//  Created by scholar on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct aboutmeprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
