131d5812e34471186c2b9e38901750cf14ed59ee
Jesslyn Gunadi <jesslyngunadi33@gmail.com>
2023-08-10 13:22:33 -0400

    initial commit test

--- aboutmeproject/ContentView.swift
+++ aboutmeproject/ContentView.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var buttonPressed = false
-    @State private var image = "jesslyn2"
     var body: some View {
         ZStack {
             Color(hue: 0.528, saturation: 0.055, brightness: 0.946)
@@ -19,7 +18,7 @@ struct ContentView: View {
                     .font(.title)
                     .fontWeight(.semibold)
                     .foregroundColor(Color(hue: 0.528, saturation: 0.638, brightness: 0.751))
-                Image(image)
+                Image("jesslyn2")
                     .resizable(resizingMode: .stretch)
                     .aspectRatio(contentMode: .fit)
                     .frame(width: 200.0)
@@ -32,10 +31,7 @@ struct ContentView: View {
                 .tint(.yellow)
                 .cornerRadius(15)
                 if buttonPressed == true {
-                    Text("hi! i'm a current rising senior in cincinnati, ohio! i'm from thailand & singapore and i love \(Text("food").foregroundColor(Color(hue: 0.528, saturation: 0.638, brightness: 0.751))
-                        .onTapGesture {
-                            image = "thaifood"
-                        })! i'm a big \(Text("family").foregroundColor(Color(hue: 0.528, saturation: 0.638, brightness: 0.751))) person and love spending time with family & \(Text("friends").foregroundColor(Color(hue: 0.528, saturation: 0.638, brightness: 0.751))). i also love playing \(Text("field hockey").foregroundColor(Color(hue: 0.528, saturation: 0.638, brightness: 0.751))), biking, and watching \(Text("sunsets").foregroundColor(Color(hue: 0.528, saturation: 0.638, brightness: 0.751))) & \(Text("sunrises").foregroundColor(Color(hue: 0.528, saturation: 0.638, brightness: 0.751)))!")
+                    Text("hi! i'm a current rising senior in \(Text("cincinnati, ohio").foregroundColor(Color(hue: 0.528, saturation: 0.638, brightness: 0.751)))! i'm from thailand & singapore and i love \(Text("food").foregroundColor(Color(hue: 0.528, saturation: 0.638, brightness: 0.751)))! i'm a big \(Text("family").foregroundColor(Color(hue: 0.528, saturation: 0.638, brightness: 0.751))) person and love spending time with family & \(Text("friends").foregroundColor(Color(hue: 0.528, saturation: 0.638, brightness: 0.751))). i also love playing \(Text("field hockey").foregroundColor(Color(hue: 0.528, saturation: 0.638, brightness: 0.751))), biking, and watching \(Text("sunsets").foregroundColor(Color(hue: 0.528, saturation: 0.638, brightness: 0.751))) & \(Text("sunrises").foregroundColor(Color(hue: 0.528, saturation: 0.638, brightness: 0.751)))!")
                         .multilineTextAlignment(.center)
                         .padding(.top, 10)
                         .padding(.horizontal, 10)
